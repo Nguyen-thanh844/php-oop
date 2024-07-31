21119089f605b6d7c188e2b8e71afed8f3551cd1
Nguyen-thanh844 <thanhntph38561@fpt.edu.vn>
2024-07-31 15:25:24 +0700

    theme-client

---
+++ src/Views/Client/layouts/partials/css.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="{{ asset('assets/client/css/bootstrap.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/font-awesome.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/elegant-icons.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/nice-select.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/jquery-ui.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/owl.carousel.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/slicknav.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('assets/client/css/style.css') }}" type="text/css">
